a265fe8d6af88289df0bf21ff4b96aa0b9479c48
MCheping8108 <liuyiming53@outlook.com>
2023-12-15 19:38:31 +0800

    del all

--- JavaScript/script.js
+++
@@ -1,3 +0,0 @@
-alert("找不到该团队")
-// Context from Function c:/Users/liuyi/Desktop/project/falldown/index.html:oncontextmenu
-document.write('找不到这个团队，如果你找到了请发我邮箱👉<a href="mailto:2480591482@qq.com">2480591482@qq.com</a>')
